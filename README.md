672bbd3132039fb85036d6e69f9d564ca0ea99a4
ysicing <i@ysicing.me>
2019-09-16 17:48:56 +0800

    update version

--- README.md
+++ README.md
@@ -3,8 +3,23 @@
 # hackbin
 常见二进制或者脚本
 
+## 常用工具
+
+```bash
+etcd v3.4.0
+helm v3.0.0-beta.3
+frp v0.29.0
+docker-compose 1.25.0-rc2
+calicoctl v3.9.0
+ctop 0.7.2
+brook v20190601
+kustomize 3.1.0
+trivy 0.1.6
+```
+
 # usage
 
 ```
 docker run --rm -v /usr/local/bin:/sysdir spanda/pkg tar zxf /pkg.tgz -C /sysdir
 ```
+
--- release.sh
+++ release.sh
@@ -14,101 +14,109 @@ get_localbin(){
 }
 
 get_etcd(){
-    local ETCD_VER=v3.3.13
-    local GOOGLE_URL=https://storage.googleapis.com/etcd
-    local GITHUB_URL=https://github.com/coreos/etcd/releases/download
-    DOWNLOAD_URL=${GOOGLE_URL}
+    local etcd_ver=v3.4.0
+    local google_url=https://storage.googleapis.com/etcd
+    # local github_url=https://github.com/coreos/etcd/releases/download
+    download_url=${google_url}
 
-    rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+    rm -f /tmp/etcd-${etcd_ver}-linux-amd64.tar.gz
     rm -rf /tmp/test-etcd && mkdir -p /tmp/test-etcd
 
-    curl -s -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
-    tar xzf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/test-etcd --strip-components=1
+    curl -s -L ${download_url}/${etcd_ver}/etcd-${etcd_ver}-linux-amd64.tar.gz -o /tmp/etcd-${etcd_ver}-linux-amd64.tar.gz
+    tar xzf /tmp/etcd-${etcd_ver}-linux-amd64.tar.gz -C /tmp/test-etcd --strip-components=1
     echo "copy etcd"
     cp /tmp/test-etcd/etcd*  ${releasedir}
     chmod +x ${releasedir}/etcd*
 }
 
 get_helm(){
-    local HELM_VER=v2.13.1
-    rm -f /tmp/helm-${HELM_VER}-linux-amd64.tar.gz
+    local helm_ver=v3.0.0-beta.3
+    rm -f /tmp/helm-${helm_ver}-linux-amd64.tar.gz
     rm -rf /tmp/helm && mkdir -p /tmp/helm 
-    curl -s -L https://storage.googleapis.com/kubernetes-helm/helm-${HELM_VER}-linux-amd64.tar.gz -o /tmp/helm-${HELM_VER}-linux-amd64.tar.gz
-    tar xzf /tmp/helm-${HELM_VER}-linux-amd64.tar.gz -C /tmp/helm  --strip-components=1
+    curl -s -L https://storage.googleapis.com/kubernetes-helm/helm-${helm_ver}-linux-amd64.tar.gz -o /tmp/helm-${helm_ver}-linux-amd64.tar.gz
+    tar xzf /tmp/helm-${helm_ver}-linux-amd64.tar.gz -C /tmp/helm  --strip-components=1
     echo "copy helm"
     cp -a /tmp/helm/helm ${releasedir}
     cp -a /tmp/helm/tiller ${releasedir}
 }
 
 get_frpx(){
-    local FRPX_VER=0.27.0
-    rm -f /tmp/frp_${FRPX_VER}_linux_amd64.tar.gz
+    local frpx_ver=0.29.0
+    rm -f /tmp/frp_${frpx_ver}_linux_amd64.tar.gz
     rm -rf /tmp/frpx && mkdir -p /tmp/frpx
-    curl -s -L https://github.com/fatedier/frp/releases/download/v${FRPX_VER}/frp_${FRPX_VER}_linux_amd64.tar.gz -o /tmp/frp_${FRPX_VER}_linux_amd64.tar.gz
-    tar xzf /tmp/frp_${FRPX_VER}_linux_amd64.tar.gz -C /tmp/frpx --strip-components=1
+    curl -s -L https://github.com/fatedier/frp/releases/download/v${frpx_ver}/frp_${frpx_ver}_linux_amd64.tar.gz -o /tmp/frp_${frpx_ver}_linux_amd64.tar.gz
+    tar xzf /tmp/frp_${frpx_ver}_linux_amd64.tar.gz -C /tmp/frpx --strip-components=1
     echo "copy frps/frpc"
     cp -a /tmp/frpx/frps ${releasedir}
     cp -a /tmp/frpx/frpc ${releasedir}
 }
 get_dockercompose(){
-    local DC_VER=1.24.0
-    curl -L https://github.com/docker/compose/releases/download/${DC_VER}/docker-compose-Linux-x86_64 -o ${releasedir}/docker-compose
-    echo "download docker-compose ${DC_VER}"
+    local dc_ver=1.25.0-rc2
+    curl -L https://github.com/docker/compose/releases/download/${dc_ver}/docker-compose-Linux-x86_64 -o ${releasedir}/docker-compose
+    echo "download docker-compose ${dc_ver}"
     chmod +x ${releasedir}/docker-compose
 }
 
 get_calicoctl(){
-    local CALICO_VER=v3.7.0
-    curl -s -L https://github.com/projectcalico/calicoctl/releases/download/${CALICO_VER}/calicoctl-linux-amd64 -o ${releasedir}/calicoctl
-    echo "download calicoctl ${CALICO_VER}"
+    local calico_ver=v3.9.0
+    curl -s -L https://github.com/projectcalico/calicoctl/releases/download/${calico_ver}/calicoctl-linux-amd64 -o ${releasedir}/calicoctl
+    echo "download calicoctl ${calico_ver}"
     chmod +x ${releasedir}/calicoctl
 }
 
 get_ctop(){
-    local CTOP_VER=0.7.2
-    curl -s -L https://github.com/bcicen/ctop/releases/download/v${CTOP_VER}/ctop-${CTOP_VER}-linux-amd64 -o ${releasedir}/ctop
-    echo "download ctop ${CTOP_VER}"
+    local ctop_ver=0.7.2
+    curl -s -L https://github.com/bcicen/ctop/releases/download/v${ctop_ver}/ctop-${ctop_ver}-linux-amd64 -o ${releasedir}/ctop
+    echo "download ctop ${ctop_ver}"
     chmod +x ${releasedir}/ctop
 }
 
-get_dry(){
-    local DRY_VER=v0.9-beta.10
-    curl -s -L https://github.com/moncho/dry/releases/download/${DRY_VER}/dry-linux-amd64 -o ${releasedir}/dry
-    echo "download dry ${DRY_VER}"
-    chmod +x ${releasedir}/dry
-}
+#get_dry(){
+#    local dry_ver=v0.9-beta.10
+#    curl -s -L https://github.com/moncho/dry/releases/download/${dry_ver}/dry-linux-amd64 -o ${releasedir}/dry
+#    echo "download dry ${dry_ver}"
+#    chmod +x ${releasedir}/dry
+#}
 
-get_imgreg(){
-    local REG_VER=v0.16.0
-    local IMG_VER=v0.5.6
-    curl -s -L https://github.com/genuinetools/reg/releases/download/${REG_VER}/reg-linux-amd64 -o ${releasedir}/reg
-    curl -s -L https://github.com/genuinetools/img/releases/download/${IMG_VER}/img-linux-amd64 -o ${releasedir}/img
-    echo "download img/reg tools"
-    chmod +x ${releasedir}/reg
-    chmod +x ${releasedir}/img
-}
+#get_imgreg(){
+#    local reg_ver=v0.16.0
+#    local img_ver=v0.5.6
+#    curl -s -L https://github.com/genuinetools/reg/releases/download/${reg_ver}/reg-linux-amd64 -o ${releasedir}/reg
+#    curl -s -L https://github.com/genuinetools/img/releases/download/${img_ver}/img-linux-amd64 -o ${releasedir}/img
+#    echo "download img/reg tools"
+#    chmod +x ${releasedir}/reg
+#    chmod +x ${releasedir}/img
+#}
 
-get_kubeprompt(){
-    curl -s -L https://rainbond-pkg.oss-cn-shanghai.aliyuncs.com/util/kube-prompt -o ${releasedir}/kube-prompt
-    chmod +x ${releasedir}/kube-prompt
-}
+#get_kubeprompt(){
+#    curl -s -L https://rainbond-pkg.oss-cn-shanghai.aliyuncs.com/util/kube-prompt -o ${releasedir}/kube-prompt
+#    chmod +x ${releasedir}/kube-prompt
+#}
 
 get_brook(){
-    local BROOK_VER=v20190401
-    curl -s -L https://github.com/txthinking/brook/releases/download/${BROOK_VER}/brook -o ${releasedir}/brook
+    local brook_ver=v20190601
+    curl -s -L https://github.com/txthinking/brook/releases/download/${brook_ver}/brook -o ${releasedir}/brook
     chmod +x ${releasedir}/brook
 }
 
 get_kustomize(){
-    local KUSTOMIZE_VER=2.0.3
-    curl -s -L https://github.com/kubernetes-sigs/kustomize/releases/download/v${KUSTOMIZE_VER}/kustomize_${KUSTOMIZE_VER}_linux_amd64 -o ${releasedir}/kustomize
+    local kustomize_ver=3.1.0
+    curl -s -L https://github.com/kubernetes-sigs/kustomize/releases/download/v${kustomize_ver}/kustomize_${kustomize_ver}_linux_amd64 -o ${releasedir}/kustomize
     chmod +x ${releasedir}/kustomize
 }
 
-get_k8s(){
-    local k8s_ver=v1.14.1
-    curl -s -L https://storage.googleapis.com/kubernetes-release/release/$k8s_ver/bin/linux/amd64/hyperkube -o ${releasedir}/hyperkube
-    chmod +x ${releasedir}/hyperkube
+#get_k8s(){
+#    local k8s_ver=v1.14.1
+#    curl -s -L https://storage.googleapis.com/kubernetes-release/release/${k8s_ver}/bin/linux/amd64/hyperkube -o ${releasedir}/hyperkube
+#    chmod +x ${releasedir}/hyperkube
+#}
+
+get_trivy(){
+    local trivy_ver=0.1.6
+    curl -s -L https://github.com/aquasecurity/trivy/releases/download/v${trivy_ver}/trivy_${trivy_ver}_Linux-64bit.tar.gz  -o /tmp/trivy_${trivy_ver}_Linux-64bit.tar.gz
+    tar xzf /tmp/trivy_${trivy_ver}_Linux-64bit.tar.gz -C /tmp/trivy --strip-components=1
+    echo "copy trivy"
+    cp -a /tmp/trivy/trivy ${releasedir}
 }
 
 download(){
@@ -119,12 +127,13 @@ download(){
     get_dockercompose
     get_calicoctl
     get_ctop
-    get_dry
-    get_imgreg
-    get_kubeprompt
+   #get_dry
+    #get_imgreg
+    #get_kubeprompt
     get_brook
     get_kustomize
-    get_k8s
+    #get_k8s
+    get_trivy
     ls -al ${releasedir}/*
 }
 
