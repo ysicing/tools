fe3382ec6dfa7bdcca70bcb06fb466439355a962
ysicing <i@ysicing.me>
2019-09-26 22:28:57 +0800

    fix etcd dl

--- release.sh
+++ release.sh
@@ -18,15 +18,19 @@ get_localbin(){
 }
 
 get_etcd(){
-    local etcd_ver=v3.4.1
-    local google_url=https://storage.googleapis.com/etcd
-    download_url=${google_url}
 
-    rm -f /tmp/etcd-${etcd_ver}-linux-amd64.tar.gz
+    ETCD_VER=v3.4.1
+
+    # choose either URL
+    GOOGLE_URL=https://storage.googleapis.com/etcd
+    GITHUB_URL=https://github.com/coreos/etcd/releases/download
+    DOWNLOAD_URL=${GOOGLE_URL}
+
+    rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
     rm -rf /tmp/test-etcd && mkdir -p /tmp/test-etcd
 
-    curl -s -L ${download_url}/${etcd_ver}/etcd-${etcd_ver}-linux-amd64.tar.gz -o /tmp/etcd-${etcd_ver}-linux-amd64.tar.gz
-    tar xzf /tmp/etcd-${etcd_ver}-linux-amd64.tar.gz -C /tmp/test-etcd --strip-components=1
+    curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+    tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/test-etcd --strip-components=1
     echo "copy etcd"
     cp /tmp/test-etcd/etcdctl  ${releasedir}
     chmod +x ${releasedir}/etcd*
