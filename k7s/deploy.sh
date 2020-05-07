9b5da49399bd0f49f7e6e1dc75be0665f60214f3
ysicing <i@ysicing.me>
2020-05-07 22:59:57 +0800

    Update deploy.sh

--- k7s/deploy.sh
+++ k7s/deploy.sh
@@ -4,11 +4,12 @@ if [ ! -z "$1" ]; then
     exec $@
 fi
 
-MASTER_IP=${MASTER_IP:-192.168.100.101}
+MASTER_IP=${MASTER_IP:-11.11.11.11}
+NODE_IP=${NODE_IP:-MASTER_IP}
 
 if [ ! -z "$PASS" ]; then
-    sealos init --passwd ${PASS} --repo registry.cn-hangzhou.aliyuncs.com/google_containers --master ${MASTER_IP} --version 1.18.2 --pkg-url /kube.tgz
+    sealos init --passwd ${PASS} --repo registry.cn-hangzhou.aliyuncs.com/google_containers --master ${MASTER_IP} --node ${NODE_IP} --version 1.18.2 --pkg-url /kube.tgz
 else
-    sealos init --repo registry.cn-hangzhou.aliyuncs.com/google_containers --master ${MASTER_IP} --version 1.18.2 --pkg-url /kube.tgz
+    sealos init --repo registry.cn-hangzhou.aliyuncs.com/google_containers --master ${MASTER_IP} --node ${NODE_IP} --version 1.18.2 --pkg-url /kube.tgz
 fi
 
