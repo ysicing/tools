c0896dc5e451aa07eefb07426258e1d0d61b70a9
ysicing <i@ysicing.me>
2020-06-25 17:22:46 +0800

    improvement: update code

--- k7s/deploy.sh
+++ k7s/deploy.sh
@@ -6,6 +6,11 @@ fi
 
 MASTER_IP=${MASTER_IP:-11.11.11.11}
 MTU=${MTU:-1440}
+K8sVersion=${K8sVersion:-1.18.4}
+
+if [ "$K8sVersion"x == "latest"x ]; then
+    K8sVersion=1.18.4
+fi
 
 if [ -z $NODE_IP ]; then
     OPTCMD="--master ${MASTER_IP}"
@@ -14,8 +19,8 @@ else
 fi
 
 if [ ! -z "$PASS" ]; then
-    sealos init --passwd ${PASS} --mtu ${MTU}  --repo registry.cn-beijing.aliyuncs.com/k7scn ${OPTCMD} --version 1.18.4 --pkg-url /kube.tgz
+    sealos init --passwd ${PASS} --mtu ${MTU}  --repo registry.cn-beijing.aliyuncs.com/k7scn ${OPTCMD} --version ${K8sVersion} --pkg-url /kube.tgz
 else
-    sealos init --mtu ${MTU} --repo registry.cn-beijing.aliyuncs.com/k7scn ${OPTCMD} --version 1.18.4 --pkg-url /kube.tgz
+    sealos init --mtu ${MTU} --repo registry.cn-beijing.aliyuncs.com/k7scn ${OPTCMD} --version ${K8sVersion} --pkg-url /kube.tgz
 fi
 
