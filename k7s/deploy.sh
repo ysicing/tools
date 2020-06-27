3f88326df3d14ced3f9c396444b152183d1f9bdd
ysicing <i@ysicing.me>
2020-06-27 22:03:30 +0800

    update k8s version

--- k7s/deploy.sh
+++ k7s/deploy.sh
@@ -6,10 +6,10 @@ fi
 
 MASTER_IP=${MASTER_IP:-11.11.11.11}
 MTU=${MTU:-1440}
-K8sVersion=${K8sVersion:-1.18.4}
+K8sVersion=${K8sVersion:-1.18.5}
 
 if [ "$K8sVersion"x == "latest"x ]; then
-    K8sVersion=1.18.4
+    K8sVersion=1.18.5
 fi
 
 if [ -z $NODE_IP ]; then
--- k7s/release.sh
+++ k7s/release.sh
@@ -2,10 +2,12 @@
 
 set -ex
 
+k8s_ver=${K8SVER:-v1.18.5}
+
 base=${1:-/kube}
 
 get_kube(){
-    local k8s_ver=v1.18.4
+    # local k8s_ver=v1.18.4
     rm -f /tmp/kubernetes-${k8s_ver}-server-linux-amd64.tar.gz
     rm -rf /tmp/k8s && mkdir -p /tmp/k8s 
     curl -s -L https://dl.k8s.io/${k8s_ver}/kubernetes-server-linux-amd64.tar.gz -o /tmp/kubernetes-${k8s_ver}-server-linux-amd64.tar.gz
