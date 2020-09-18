c315a1d3e1a5432a1e5c9f1f73d45664e9768f20
ysicing <i@ysicing.me>
2020-09-18 14:32:02 +0800

    fix: fix ci

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 set -e
+
 images=$(ls -al | grep "drwxr"  | grep -v "\." | awk '{print $NF}' | tr '\n' ' ')
 for image in ${images[@]}
 do
     cat ${image}/Dockerfile | grep FROM | awk '{print $2}' | xargs -I {} docker pull {}
-    docker build -t ysicing/${image} ${image}
+    if [ ${image} != "k7s" ]; then  
+        docker build -t ysicing/${image} ${image}
+    else
+        docker build -t ysicing/${image} -f ${image}/Dockerfile .
+    fi    
     docker push ysicing/${image}
-    echo ${image}
     curl -s https://cr.hk2.godu.dev/pull\?name="ysicing/${image}"
 done
