8c9144eee96a10357a5da273ba6607232146a208
ysicing <i@ysicing.me>
2020-10-12 10:19:11 +0800

    feat: update code

--- k7s/deploy.sh
+++ k7s/deploy.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+set -e
+
 if [ "$1" == "bash" ]; then
     exec /bin/bash
 fi
 
 if [ "$1" == "init" ]; then
-    [ -f "/root/.sealos/kube.tgz" ] && rm -rf /root/.sealos/kube.tgz || mkdir -p /root/.sealos
+    [ -d "/root/.sealos" ] && (
+        # [ -f "/root/.sealos/kube.tgz" ] && rm -rf /root/.sealos/kube.tgz
+        mv /root/.sealos /root/.sealos.$(date +%Y%m%d%H)
+    )
+    mkdir -p /root/.sealos
     cp -a /kube.tgz /root/.sealos/kube.tgz
     exec sealos init --repo registry.cn-beijing.aliyuncs.com/k7scn --version 1.19.2 --pkg-url /root/.sealos/kube.tgz ${@:2}
 else
