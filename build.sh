48ee36ca407e707c983123045e0ef3a6d0552387
缘生 <i@ysicing.me>
2021-02-02 22:24:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 images=$(ls -al | grep "drwxr"  | grep -v "\." | awk '{print $NF}' | tr '\n' ' ')
 for image in ${images[@]}
 do
-    cat ${image}/Dockerfile | grep FROM | awk '{print $2}' | xargs -I {} docker pull {}
+    # cat ${image}/Dockerfile | grep FROM | awk '{print $2}' | xargs -I {} docker pull {}
     if [ ${image} == "pkg" ]; then  
         docker build -t ysicing/${image}:1.19.7 ${image}
         docker push ysicing/${image}:1.19.7
