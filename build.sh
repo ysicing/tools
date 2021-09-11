8f8c96880f93c5bbb40b8d21bbf06579013ecbdb
ysicing <i@ysicing.me>
2021-09-11 21:37:30 +0800

    fix(tools): imp tools
    imp tools
    
    Signed-off-by: ysicing <i@ysicing.me>

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 images=$(ls -al | grep "drwxr"  | grep -v "\." | awk '{print $NF}' | tr '\n' ' ')
 for image in ${images[@]}
 do
-    cat ${image}/Dockerfile | grep FROM | awk '{print $2}' | xargs -I {} docker pull {}
+    cat ${image}/Dockerfile | grep -E "^FROM" | awk '{print $2}' | xargs -I {} docker pull {}
     if [ ${image} != "k7s" -a ${image} != "pkg" ]; then
         docker build -t ysicing/${image} ${image}
     else
